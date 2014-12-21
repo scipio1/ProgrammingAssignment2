833b47f44492b54bbb995f35a7f086de25b09c7c
scipio1 <wk0801@163.com>
2014-12-21 23:17:36 +0800

    work

--- cachematrix.R
+++ cachematrix.R
@@ -5,30 +5,29 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
-        set <- function(y){
+        set <- function(y){     ##set函数用于将y的值赋予x
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setsolve <- function(solve) m <<- solve
-        getsolve <- function() m
+        get <- function() x    ##get函数用于返回x的值
+        setsolve <- function(solve) m <<- solve         ##setsolve函数用于将solve的值赋予m
+        getsolve <- function() m        ##get函数用于返回m的值
         list(set = set, get = get,
              setsolve = setsolve,
-             getsolve = getsolve)
+             getsolve = getsolve)       ##返回list
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        m <- x$getsolve()
-        if(!is.null(m)){
+        m <- x$getsolve()       ##尝试读取缓存的逆矩阵
+        if(!is.null(m)){        ##判断缓存的逆矩阵是否为null
                 message("getting cached data")
-                return(m)
+                return(m)       ##如果不为null，直接输出缓存值
         }
-        data <- x$get()
-        m <- solve(data)
-        x$setsolve(m)
+        data <- x$get()         ##如果为null，则读取原矩阵
+        m <- solve(data)        ##求出逆矩阵
+        x$setsolve(m)           ##将值存放到缓存中
         m
-        ## Return a matrix that is the inverse of 'x'
 }
